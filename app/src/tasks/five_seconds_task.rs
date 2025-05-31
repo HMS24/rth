d13fe035d7e4d8471f2221927ec711bca707a029
chenrohua24 <z7707092003@gmail.com>
2025-05-31 17:43:14 +0800

    refactor: update FiveSecondsTask to use async job creation and include task ID in logging

--- app/src/tasks/five_seconds_task.rs
+++ app/src/tasks/five_seconds_task.rs
@@ -7,10 +7,12 @@ pub struct FiveSecondsTask;
 
 impl CronJob for FiveSecondsTask {
     fn create() -> Result<Job, JobSchedulerError> {
-        Job::new("*/5 * * * * *", |_uuid, _lock| {
-            let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        Job::new_async("*/5 * * * * *", |uuid, _lock| {
+            Box::pin(async move {
+                let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
 
-            println!("[{}] I run every 5 seconds!", now);
+                println!("[{}] I run every 5 seconds! Task id: {}", now, uuid);
+            })
         })
     }
 
